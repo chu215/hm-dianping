ea6660a1f70059d679af6df671ff24867f6a534a
chu <2254886767@qq.com>
2022-11-10 11:15:37 +0800

    测试UV统计

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -42,4 +42,21 @@ class HmDianPingApplicationTests {
             stringRedisTemplate.opsForGeo().add(key, locations);
         }
     }
+
+    @Test
+    void testHyperLogLog() {
+        String[] values = new String[1000];
+        int j = 0;
+        for (int i = 0; i < 1000000; i++) {
+            j = i % 1000;
+
+            values[j] = "user_" + i;
+            if (j == 999) {
+                stringRedisTemplate.opsForHyperLogLog().add("hl1", values);
+            }
+        }
+
+        Long size = stringRedisTemplate.opsForHyperLogLog().size("hl1");
+        System.out.println("size = " + size);
+    }
 }
